41c5a48b7e2984468f3214adb42a30c326d7066d
VaibhavTiw <vaibhav.tiw543@gmail.com>
2024-06-16 00:59:08 +0530

    footer changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="flex items-center justify-center w-full pt-6 border-t border-neutral-100 dark:border-neutral-800">
       <div className="container flex items-center justify-between">
         <div className="text-xs text-neutral-500">
-          Built by Amaan Bhati | ©2024 <a className="font-bold text-pink" href="https://amaanbhati.tech">amaanbhati.tech</a> All Rights Reserved
+          Built by Vaibhav Tiwari | ©2024 <a className="font-bold text-pink" ></a> All Rights Reserved
         </div>
         <ThemeToggle />
       </div>
